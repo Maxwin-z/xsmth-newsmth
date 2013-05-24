23976d676607b7741cb679a448332666b3e935cc
wendong.zhang <wendong.zhang@dianping.com>
2013-05-24 17:59:08 +0800

    add asi framework

--- newsmth/ViewController.m
+++ newsmth/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "ASIHTTPRequest.h"
 
-@interface ViewController ()
+@interface ViewController ()<ASIHTTPRequestDelegate>
 
 @end
 
@@ -18,12 +19,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    ASIHTTPRequest *req = [[ASIHTTPRequest alloc] initWithURL:[NSURL URLWithString:@"http://www.newsmth.net/bbscon.php?bid=133&id=1936479557"]];
+    req.delegate = self;
+    [req startAsynchronous];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - ASIHTTPRequestDelegate
+- (void)requestFinished:(ASIHTTPRequest *)request
+{
+    NSLog(@"rsp:%@", request.responseString);
 }
 
 @end
