089f583ae9ccf49801f1f89f7404fa820ee0edff
wendong.zhang <wendong.zhang@dianping.com>
2013-06-13 17:49:52 +0800

    add favor page

--- newsmth/SMFavorListViewController.m
+++ newsmth/SMFavorListViewController.m
@@ -8,11 +8,18 @@
 
 #import "SMFavorListViewController.h"
 #import "SMLoginViewController.h"
+#import "XPullRefreshTableView.h"
+#import "SMFavor.h"
+#import "SMBoard.h"
 
 static SMFavorListViewController *_instance;
 
-@interface SMFavorListViewController ()
+@interface SMFavorListViewController ()<SMWebLoaderOperationDelegate, XPullRefreshTableViewDelegate, UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet XPullRefreshTableView *tableView;
+@property (weak, nonatomic) IBOutlet UIButton *buttonForLogin;
 
+@property (strong, nonatomic) SMWebLoaderOperation *favorListOp;
+@property (strong, nonatomic) NSArray *boards;
 @end
 
 @implementation SMFavorListViewController
@@ -25,11 +32,77 @@ static SMFavorListViewController *_instance;
     return _instance;
 }
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"收藏";
+    _tableView.xdelegate = self;
+    if ([SMAccountManager instance].isLogin) {
+        _tableView.hidden = NO;
+        _buttonForLogin.hidden = YES;
+        [_tableView beginRefreshing];
+    } else {
+        _tableView.hidden = YES;
+        _buttonForLogin.hidden = NO;
+    }
+}
+
+- (void)loadData
+{
+    _favorListOp = [[SMWebLoaderOperation alloc] init];
+    _favorListOp.delegate = self;
+    [_favorListOp loadUrl:@"http://m.newsmth.net/favor" withParser:@"favor"];
+}
+
+- (void)setBoards:(NSArray *)boards
+{
+    _boards = boards;
+    [self.tableView reloadData];
+}
+
 - (IBAction)onLoginButtonClick:(id)sender
 {
     SMLoginViewController *loginVc = [[SMLoginViewController alloc] init];
     UINavigationController *nvc = [[UINavigationController alloc] initWithRootViewController:loginVc];
     [self.navigationController presentModalViewController:nvc animated:YES];
+}
+
+#pragma mark - UITableDelegate/DataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _boards.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellId = @"boardcell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    SMBoard *board = _boards[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@(%@)", board.cnName, board.name];
+    return cell;
+}
+
+#pragma mark - XPullRefreshTableViewDelegate
+- (void)tableViewDoRefresh:(XPullRefreshTableView *)tableView
+{
+    [self loadData];
+}
+
+#pragma mark - SMWebLoaderOperationDelegate
+- (void)webLoaderOperationFinished:(SMWebLoaderOperation *)opt
+{
+    [_tableView endRefreshing:YES];
+    SMFavor *favor = opt.data;
+    self.boards = favor.boards;
+}
+
+- (void)webLoaderOperationFail:(SMWebLoaderOperation *)opt error:(SMMessage *)error
+{
+    [_tableView endRefreshing:NO];
+    [self toast:error.message];
 }
 
 @end
