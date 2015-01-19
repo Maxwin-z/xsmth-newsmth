79037d5e1207c9ffb12d3ec0c0c57e7968de5702
Maxwin-z <zwd2005@gmail.com>
2015-01-19 21:23:21 +0800

    add touch.js

--- template_posts/pack.sh
+++ template_posts/pack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm template_posts.zip
 
-zip template_posts.zip index.html SMApp.js SMAppControl.js placeholder.jpg zepto.js icon_loading.png icon_action.png 
+zip template_posts.zip index.html SMApp.js SMAppControl.js placeholder.jpg zepto.js touch.js icon_loading.png icon_action.png 
 
