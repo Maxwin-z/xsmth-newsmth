ad457e4a92633e3e7c8777cdebd085083bac6501
Maxwin-z <zwd2005@gmail.com>
2015-10-08 13:37:34 +0800

    fix photopicker width

--- newsmth/Common/XImagePicker/SMImagePickerAssetsCell.m
+++ newsmth/Common/XImagePicker/SMImagePickerAssetsCell.m
@@ -9,6 +9,9 @@
 #import "SMImagePickerAssetsCell.h"
 #import <AssetsLibrary/ALAsset.h>
 
+
+#define PADDING 4.0f
+
 @interface SMImagePickerAssetsCell ()
 @property (strong, nonatomic) IBOutlet UIView *viewForCell;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *imageButton;
@@ -19,7 +22,8 @@
 
 + (CGFloat)cellHeight
 {
-    return 83.0f;
+    int cols = 4;
+    return ([UIScreen mainScreen].bounds.size.width - (cols + 1) * PADDING) / cols + PADDING;
 }
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
@@ -38,6 +42,7 @@
 
 - (void)setAssets:(NSArray *)assets start:(NSInteger)start;
 {
+    int width = [[self class] cellHeight] - PADDING;
     int i = 0;
     for (; i + start != assets.count && i != _imageButton.count; ++i) {
         UIButton *button = _imageButton[i];
@@ -45,6 +50,11 @@
         [button setImage:[UIImage imageWithCGImage:asset.thumbnail] forState:UIControlStateNormal];
         button.tag = i + start;
         button.hidden = NO;
+        
+        CGRect frame = button.frame;
+        frame.size = CGSizeMake(width, width);
+        frame.origin.x = i * (width + PADDING) + PADDING;
+        button.frame = frame;
     }
     for (; i < _imageButton.count; ++i) {
         UIButton *button = _imageButton[i];
