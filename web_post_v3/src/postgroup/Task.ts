2ad42e86f92f8859890b316c570a9243dcdbdaf2
Maxwin-z <zwd2005@gmail.com>
2020-02-21 13:41:31 +0800

    feat: single post

---
+++ web_post_v3/src/postgroup/Task.ts
@@ -0,0 +1,94 @@
+import { ajax } from "../jsbridge";
+import {
+  isErrorPage,
+  retrieveGroupPosts,
+  delay,
+  formatPost,
+  cleanHtml
+} from "./postUtils";
+import { PostGroup, Post } from "./types";
+export class GroupTask {
+  board: string;
+  gid: number;
+  page: number;
+  resolve?: ((value: PostGroup) => void) | null;
+  reject?: ((reason?: any) => void) | null;
+  constructor(board: string, gid: number, page: number = 1) {
+    this.board = board;
+    this.gid = gid;
+    this.page = page;
+  }
+
+  execute(): Promise<PostGroup> {
+    return new Promise(async (resolve, reject) => {
+      this.reject = reject;
+      // await delay(3000);
+      const html = await ajax({
+        url: `https://www.newsmth.net/nForum/article/${this.board}/${this.gid}?ajax&p=${this.page}`,
+        headers: {
+          "X-Requested-With": "XMLHttpRequest"
+        }
+      });
+      const error = isErrorPage(html);
+      if (error) {
+        reject(error);
+        this.reject = null;
+      }
+      resolve(retrieveGroupPosts(html));
+    });
+  }
+
+  cancel() {
+    if (this.reject) {
+      this.reject("request cancel");
+      this.reject = null;
+    }
+  }
+}
+
+export class PostTask {
+  board: string;
+  pid: number;
+  reject?: ((reason?: any) => void) | null;
+  constructor(board: string, pid: number) {
+    this.board = board;
+    this.pid = pid;
+  }
+
+  execute(): Promise<Post> {
+    return new Promise(async (resolve, reject) => {
+      this.reject = reject;
+      const html = await ajax({
+        url: `https://www.newsmth.net/nForum/article/${this.board}/ajax_single/${this.pid}.json`,
+        headers: {
+          "X-Requested-With": "XMLHttpRequest"
+        }
+      });
+      let data = JSON.parse(html);
+      (data.content as string).match(/.{1,100}/g)?.forEach(v => console.log(v));
+      data = { ...data, ...formatPost(cleanHtml(data.content)) };
+      const post: Post = {
+        board: data["board_name"],
+        gid: data["group_id"],
+        pid: data["id"],
+        title: data["title"],
+        author: data["user"]["id"],
+        nick: data["user"]["user_name"],
+        floor: -1,
+        date: data["date"],
+        dateString: data["dateString"],
+        content: data["content"],
+        images: data.images,
+        isSingle: true
+      };
+      resolve(post);
+    });
+  }
+
+  cancel() {
+    if (this.reject) {
+      this.reject("request cancel");
+      this.reject = null;
+    }
+  }
+}
---
+++ web_post_v3/src/tests/Task.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { GroupTask, PostTask } from "../postgroup/Task";
+import "./test.css";
+export default function TaskTest() {
+  let task: GroupTask;
+  let postTask: PostTask;
+
+  async function click() {
+    task = new GroupTask("WorkLife", 2177468, 2);
+    const postgroup = await task.execute();
+    console.log(postgroup);
+  }
+  async function click2() {
+    // postTask = new PostTask("WorkLife", 2177468);
+    postTask = new PostTask("Picture", 2185856);
+    const post = await postTask.execute();
+    console.log(post);
+  }
+
+  function cancel() {
+    task && task.cancel();
+    postTask && postTask.cancel();
+  }
+  return (
+    <div>
+      <button onClick={click}>fetch posts</button>
+      <button onClick={click2}>fetch post</button>
+      <button onClick={cancel}>cancel</button>
+    </div>
+  );
+}
