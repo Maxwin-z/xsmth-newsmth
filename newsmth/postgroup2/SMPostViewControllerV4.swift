b6d694e82caf1d56d6308fd6e7b7f10d4dc044db
Maxwin-z <zwd2005@gmail.com>
2020-05-09 09:53:06 +0800

    feat: v4.1.0

--- newsmth/postgroup2/SMPostViewControllerV4.swift
+++ newsmth/postgroup2/SMPostViewControllerV4.swift
@@ -40,8 +40,8 @@ class SMPostViewControllerV4: XWebController {
 
     override func viewDidLoad() {
         if post != nil {
-            url = URL(string: "http://10.0.0.11:3000/#/")
-//            url = URL(string: "http://public-1255362875.cos.ap-shanghai.myqcloud.com/xsmth/v4.1.0/index.html#/")
+//            url = URL(string: "http://10.0.0.11:3000/#/")
+            url = URL(string: "http://public-1255362875.cos.ap-shanghai.myqcloud.com/xsmth/v4.1.0/index.html#/")
         }
 
         super.viewDidLoad()
