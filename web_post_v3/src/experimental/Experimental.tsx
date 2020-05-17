addfccd50f3a81b656fe838a91b7e78e803f653c
Maxwin-z <zwd2005@gmail.com>
2020-05-17 15:21:31 +0800

    feat: add sort

--- web_post_v3/src/experimental/Experimental.tsx
+++ web_post_v3/src/experimental/Experimental.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { getQuery } from "../article/utils/urlHelper";
 import { getStorage, setTitle } from "../jsapi";
 import { RootState } from "../article";
+
+import { YAxis, CartesianGrid, AreaChart, Area, Legend } from "recharts";
+
 import "./experimental.css";
 
 interface IUser {
@@ -20,10 +23,16 @@ interface ICountMap {
 setTitle("Experimental");
 
 const Experimental: FC<{}> = () => {
+  type UserKeys = "floor" | "count" | "postCount" | "score";
+
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
+  const [authorPostCount, setAuthorPostCount] = useState(0);
+  const [postsCount, setPostsCount] = useState(0);
   const [users, setUsers] = useState<IUser[]>([]);
-  const [countMap, setCountMap] = useState<ICountMap>({});
+  const [lastSortKey, setLastSortKey] = useState<UserKeys>("floor");
+  const [lastSortMethod, setLastSortMethod] = useState(1);
+
   useEffect(() => {
     async function main() {
       const query = getQuery();
@@ -35,60 +44,109 @@ const Experimental: FC<{}> = () => {
       setTitle(data.group.mainPost.title);
 
       const users: IUser[] = [];
+      let postsCount = 0;
+      let postAuthor = "";
+      const userMap: { [author: string]: IUser } = {};
       data.group.pages.forEach(page => {
         page.posts.forEach(({ postCount, author, score, floor }) => {
-          users.push({
-            author,
-            postCount,
-            score,
-            floor,
-            count: 0
-          });
+          if (!userMap[author]) {
+            const user = {
+              author,
+              postCount,
+              score,
+              floor,
+              count: 1
+            };
+            users.push(user);
+            userMap[author] = user;
+          } else {
+            ++userMap[author].count;
+          }
+          ++postsCount;
           if (floor == 0) {
-            setAuthor(author);
+            postAuthor = author;
           }
         });
       });
-
-      const countMap: ICountMap = {};
-      users.forEach(user => {
-        if (!countMap[user.author]) {
-          countMap[user.author] = user;
-        }
-        countMap[user.author].count++;
-      });
+      setAuthor(postAuthor);
+      setAuthorPostCount(userMap[postAuthor].count);
+      setPostsCount(postsCount);
       setUsers(users);
-      setCountMap(countMap);
     }
     main();
   }, []);
 
+  function sortBy(key: UserKeys) {
+    const tmp = [...users];
+    let sm = lastSortMethod;
+    if (lastSortKey === key) {
+      sm = sm * -1;
+    } else {
+      sm = -1;
+    }
+    tmp.sort((u1, u2) => {
+      return sm * (u1[key] - u2[key]);
+    });
+    setUsers(tmp);
+    setLastSortKey(key);
+    setLastSortMethod(sm);
+  }
+
+  const anchor = (key: UserKeys) => {
+    return lastSortKey === key ? (lastSortMethod == 1 ? "🔼" : "🔽") : "";
+  };
   // const data:Json = await
   return (
     <div className="main">
       <h3>{title}</h3>
       <div>
         本文作者{author}，参与互动
-        {countMap[author] ? countMap[author].count : "--"}个。共{users.length}
-        楼，{Object.keys(countMap).length}
+        {authorPostCount}个。共{postsCount}
+        楼，{users.length}
         人参与本帖。
+      </div>
+      <div className="chart">
+        <AreaChart
+          width={400}
+          height={100}
+          data={users}
+          margin={{ top: 10, right: 0, left: 0, bottom: 10 }}
+        >
+          <defs>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <YAxis />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Area
+            type="monotone"
+            dataKey="count"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
       </div>
       <div>
         <table className="userlist">
           <thead>
             <tr>
-              <th>作者🔼</th>
-              <th>帖子🔽</th>
-              <th>积分</th>
-              <th> 回复</th>
+              <th onClick={() => sortBy("floor")}>作者{anchor("floor")}</th>
+              <th onClick={() => sortBy("postCount")}>
+                帖子{anchor("postCount")}
+              </th>
+              <th onClick={() => sortBy("score")}>积分{anchor("score")}</th>
+              <th onClick={() => sortBy("count")}> 回复{anchor("count")}</th>
             </tr>
           </thead>
           <tbody>
-            {Object.keys(countMap).map(author => {
-              const user = countMap[author];
+            {users.map(user => {
               return (
-                <tr key={author}>
-                  <td>{author}</td>
+                <tr key={user.author}>
+                  <td>{user.author}</td>
                   <td>{user.postCount}</td>
                   <td>{user.score}</td>
                   <td>{user.count}</td>
