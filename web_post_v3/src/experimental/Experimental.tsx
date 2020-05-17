9c6f9fbcd71be167014213d1b59c6d147f317027
Maxwin-z <zwd2005@gmail.com>
2020-05-17 14:04:09 +0800

    feat: reply list

--- web_post_v3/src/experimental/Experimental.tsx
+++ web_post_v3/src/experimental/Experimental.tsx
@@ -1,25 +1,105 @@
 import { FC, useEffect, useState } from "react";
 import React from "react";
 import { getQuery } from "../article/utils/urlHelper";
-import { getStorage } from "../jsapi";
+import { getStorage, setTitle } from "../jsapi";
+import { RootState } from "../article";
+import "./experimental.css";
+
+interface IUser {
+  author: string;
+  postCount: number;
+  score: number;
+  floor: number;
+  count: number;
+}
+
+interface ICountMap {
+  [author: string]: IUser;
+}
+
+setTitle("Experimental");
 
 const Experimental: FC<{}> = () => {
-  const [data, setData] = useState({});
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [countMap, setCountMap] = useState<ICountMap>({});
   useEffect(() => {
     async function main() {
       const query = getQuery();
       const board = query.board as string;
       const gid = query.gid as string;
       const storeKey = `post_${board}_${gid}_`;
-      const data = await getStorage(storeKey);
+      const data: RootState = await getStorage(storeKey);
       console.log(data);
-      setData(data);
+      setTitle(data.group.mainPost.title);
+
+      const users: IUser[] = [];
+      data.group.pages.forEach(page => {
+        page.posts.forEach(({ postCount, author, score, floor }) => {
+          users.push({
+            author,
+            postCount,
+            score,
+            floor,
+            count: 0
+          });
+          if (floor == 0) {
+            setAuthor(author);
+          }
+        });
+      });
+
+      const countMap: ICountMap = {};
+      users.forEach(user => {
+        if (!countMap[user.author]) {
+          countMap[user.author] = user;
+        }
+        countMap[user.author].count++;
+      });
+      setUsers(users);
+      setCountMap(countMap);
     }
     main();
   }, []);
 
   // const data:Json = await
-  return <div>Experimental, {JSON.stringify(data)}</div>;
+  return (
+    <div className="main">
+      <h3>{title}</h3>
+      <div>
+        本文作者{author}，参与互动
+        {countMap[author] ? countMap[author].count : "--"}个。共{users.length}
+        楼，{Object.keys(countMap).length}
+        人参与本帖。
+      </div>
+      <div>
+        <table className="userlist">
+          <thead>
+            <tr>
+              <th>作者🔼</th>
+              <th>帖子🔽</th>
+              <th>积分</th>
+              <th> 回复</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(countMap).map(author => {
+              const user = countMap[author];
+              return (
+                <tr key={author}>
+                  <td>{author}</td>
+                  <td>{user.postCount}</td>
+                  <td>{user.score}</td>
+                  <td>{user.count}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Experimental;
