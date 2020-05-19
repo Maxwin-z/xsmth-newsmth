dcaf36676c7e84e704842e63e8510c61bb6d9add
Maxwin-z <zwd2005@gmail.com>
2020-05-19 22:53:48 +0800

    feat: add chrome.js

--- web_post_v3/chrome.js
+++ web_post_v3/chrome.js
@@ -1,4 +1,24 @@
-const fs = require('fs')
-const path = require('path')
-const manifest = fs.readFileSync(path.join(__dirname, './public/manifest.json')).toString()
+const fs = require("fs");
+const path = require("path");
+const manifest = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "./public/manifest.json")).toString()
+);
+const indexHtml = fs
+  .readFileSync(path.join(__dirname, "./build/index.html"))
+  .toString();
+const loader = indexHtml.replace(/^.*<script>(.*?)<\/script>.*$/, "$1");
+fs.writeFileSync(path.join(__dirname, "build/static/js/loader.js"), loader);
+const jsFiles = fs
+  .readdirSync(path.join(__dirname, "build/static/js/"))
+  .filter((file) => file.match(/\.js$/)).map(file => `./static/js/${file}`);
+const cssFiles = fs
+  .readdirSync(path.join(__dirname, "build/static/css/"))
+  .filter((file) => file.match(/\.css$/)).map(file => `./static/css/${file}`);
+manifest.content_scripts[0].js = jsFiles;
+manifest.content_scripts[0].css = cssFiles;
+fs.writeFileSync(
+  path.join(__dirname, "build/manifest.json"),
+  JSON.stringify(manifest, true, 2)
+);
 console.log(manifest)
+console.log("done");
