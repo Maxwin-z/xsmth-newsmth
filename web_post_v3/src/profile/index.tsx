86fcb79215f4fe3314eed8bf4f09dfd407cc798f
Maxwin-z <zwd2005@gmail.com>
2020-06-06 23:22:34 +0800

    feat: picker done

--- web_post_v3/src/profile/index.tsx
+++ web_post_v3/src/profile/index.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "@simonwep/pickr/dist/themes/classic.min.css"; // 'classic' theme
+import "@simonwep/pickr/dist/themes/monolith.min.css"; // 'monolith' theme
+
+import Pickr from "@simonwep/pickr";
+// import Pickr from "@simonwep/pickr/dist/pickr.es5.min";
+
 import "./index.css";
 
 function App() {
+  const showPicker = () => {
+    const picker = Pickr.create({
+      el: ".color-picker",
+      theme: "monolith", // or 'monolith', or 'nano'
+      //   position: "bottom-middle",
+      //   container: ".picker-container",
+      swatches: [
+        "rgba(244, 67, 54, 1)",
+        "rgba(233, 30, 99, 0.95)",
+        "rgba(156, 39, 176, 0.9)",
+        "rgba(103, 58, 183, 0.85)",
+        "rgba(63, 81, 181, 0.8)",
+        "rgba(33, 150, 243, 0.75)",
+        "rgba(3, 169, 244, 0.7)",
+        "rgba(0, 188, 212, 0.7)",
+        "rgba(0, 150, 136, 0.75)",
+        "rgba(76, 175, 80, 0.8)",
+        "rgba(139, 195, 74, 0.85)",
+        "rgba(205, 220, 57, 0.9)",
+        "rgba(255, 235, 59, 0.95)",
+        "rgba(255, 193, 7, 1)"
+      ],
+
+      components: {
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+          hex: true,
+          rgba: true,
+          hsla: true,
+          hsva: true,
+          cmyk: true,
+          input: true,
+          clear: true,
+          save: true
+        }
+      }
+    });
+  };
+
   return (
     <div>
       <div className="header flex-row">
@@ -54,7 +104,18 @@ function App() {
         </div>
         <div className="delete">➕</div>
       </div>
-      <div className="cell">➕添加Tag</div>
+      <div className="cell" onClick={showPicker}>
+        ➕添加Tag
+      </div>
+      <div
+        className="cell picker-container"
+        // style={{
+        //   justifyContent: "center",
+        //   alignItems: "center"
+        // }}
+      >
+        Picker <div className="color-picker"></div>
+      </div>
     </div>
   );
 }
